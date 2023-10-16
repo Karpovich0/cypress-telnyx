7366b0069725a8fd081592093009e88826a5c12d
Olekka <noodarius0@gmail.com>
2023-10-16 19:44:26 +0300

    add fixtures

--- cypress/e2e/telnyx.cy.js
+++ cypress/e2e/telnyx.cy.js
@@ -5,7 +5,14 @@ import contactUsPage from "./../pages/contactUsPage";
 import solutionsPage from "./../pages/solutionsPage";
 import pricingPage from "./../pages/pricingPage";
 import loginPage from "./../pages/loginPage";
+import telnyx from "./../fixtures/telnyx.json";
+import user from "./../fixtures/user.json";
 describe("Telnyx", () => {
+	beforeEach(() => {
+		cy.fixture("telnyx").as("telnyx");
+		cy.fixture("user").as("user");
+	});
+
 	it("Homepage Validation", () => {
 		cy.visitAndAcceptCookies("/");
 		homePage.elements.logo().should("be.visible");
@@ -18,23 +25,32 @@ describe("Telnyx", () => {
 			cy.get("button:first-of-type").click();
 		});
 		homePage.elements.productsVoiceApi().should("be.visible").click();
-		cy.url().should("eq", "https://telnyx.com/products/voice-api");
-		cy.get("h1").should("have.text", "Voice API");
+		telnyx;
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("eq", data.productsVoiceApiLink);
+			cy.get("h1").should("have.text", data.productsVoiceApiTitle);
+		});
 	});
 
 	it("Footer Navigation Functionality On Desktop", () => {
 		cy.visitAndAcceptCookies("/");
 		homePage.elements.footer().scrollIntoView();
 		homePage.elements.footerGlobalCoverage().click();
-		cy.url().should("eq", "https://telnyx.com/global-coverage");
-		cy.get("h1 span").should("have.text", "Global coverage");
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("eq", data.globalCoverageLink);
+			cy.get("h1 span").should("have.text", data.globalCoverageTitle);
+		});
 	});
 
 	it("Registration using invalid data", () => {
 		cy.visitAndAcceptCookies("/");
 		homePage.clickOnSignup();
-		cy.url().should("eq", "https://telnyx.com/sign-up");
-		registrationPage.fillForm("notAllowedString", "Tester", "Testov", "CorrectPasswrod123.?");
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("eq", data.signUpLink);
+		});
+		cy.get("@user").then((data) => {
+			registrationPage.fillForm(data.invalidEmail, data.firstName, data.secondName, data.password);
+		});
 		registrationPage.acceptTermsAndConditions();
 		registrationPage.submitForm();
 		registrationPage.elements.error().eq(0).should("be.visible");
@@ -44,26 +60,31 @@ describe("Telnyx", () => {
 	it("Open contact us form from products page", () => {
 		cy.visitAndAcceptCookies("/products");
 		productsPage.elements.contactUs().eq(2).click();
-		cy.url().should("eq", "https://telnyx.com/contact-us");
-		contactUsPage.elements.title().should("have.text", "Talk to an expert");
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("eq", data.contactUsLink);
+			contactUsPage.elements.title().should("have.text", data.contactUsTitle);
+		});
 	});
 	it("Scroll down functionality on Solutions' page", () => {
 		cy.visitAndAcceptCookies("/");
 		homePage.elements.solutionsLink().eq(0).click();
-		cy.url().should("eq", "https://telnyx.com/solutions");
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("eq", data.solutionsLink);
+		});
 		solutionsPage.elements.useCasesLink().click();
 		solutionsPage.elements.useCasesTitle().should("be.visible");
 	});
 	it("Check adaptivity on viewport width change", () => {
 		cy.visitAndAcceptCookies("/");
 		homePage.elements.headerBurger().should("not.be.visible");
-		cy.viewport(1439, 900);
+		Cypress.config("baseUrl", null);
+		cy.viewport("ipad-2");
 		homePage.elements.headerBurger().should("be.visible");
 	});
 	it("Burger functionality", () => {
 		cy.visitAndAcceptCookies("/");
 		// IPhone 12 pro's viewport
-		cy.viewport(390, 844);
+		cy.viewport("iphone-x");
 		homePage.elements.headerBurger().should("be.visible").click();
 		homePage.elements
 			.headerNavigationMobile()
@@ -77,13 +98,19 @@ describe("Telnyx", () => {
 			.within(() => {
 				cy.get('[href="https://developers.telnyx.com"]').invoke("attr", "target", "_self").click();
 			});
-		cy.url().should("include", "https://developers.telnyx.com");
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("include", data.developersLink);
+		});
 	});
 	it("Login with invalid data", () => {
 		cy.visitAndAcceptCookies("/");
 		homePage.elements.loginLink().should("be.visible").invoke("attr", "target", "_self").click();
-		cy.url().should("include", "https://portal.telnyx.com/");
-		loginPage.fillForm("unExistingEmail@gmail.com", "wrongPassword12.?");
+		cy.get("@telnyx").then((data) => {
+			cy.url().should("include", data.portalLink);
+		});
+		cy.get("@user").then((data) => {
+			loginPage.fillForm(data.validEmail, data.password);
+		});
 		loginPage.submitForm();
 		loginPage.elements.errorMessage().should("exist");
 	});
